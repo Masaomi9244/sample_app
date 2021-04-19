b673ab390747bc979492821dd8d5167d77115da8
Masaomi1 <mario8masaomiomi@gmail.com>
2021-04-20 07:01:35 +0900

    issue14対応完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_105253) do
+ActiveRecord::Schema.define(version: 2021_04_15_094426) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_105253) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
