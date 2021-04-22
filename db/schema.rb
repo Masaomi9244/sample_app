68e883b1103bae96977791d2f9d2d9efcd1e080a
Masaomi1 <mario8masaomiomi@gmail.com>
2021-04-23 08:17:48 +0900

    issue18対応完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_224401) do
+ActiveRecord::Schema.define(version: 2021_04_22_225457) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_224401) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
